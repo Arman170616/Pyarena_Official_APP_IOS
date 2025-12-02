b5cbd63695b4f72ee86320d6cbdc50e2e3a21aaf
Arman170616 <armanice09@gmail.com>
2025-12-02 18:08:49 +0400

    Initial Commit

---
+++ School/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  School
+//
+//  Created by FutureInnvo on 02/12/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
